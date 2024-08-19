16f33c9984464813067bc0d6b3baafcf1b5e65f0
CollinJRehmeyer <rehmeyercollinj@gmail.com>
2024-08-18 22:07:33 -0400

    Adding department manager

--- Assets/Scripts/DepartmentManager.cs
+++ Assets/Scripts/DepartmentManager.cs
@@ -5,6 +5,37 @@ using UnityEngine;
 public class DepartmentManager : MonoBehaviour
 {
     public GameObject[] departmentObjs;
+    public int newDeptIndex = 0;
     public int employeesPerFloor;
-    public int maxEmployees;
+    public int maxEmployees = 6;
+
+    public GameObject companyHead;
+
+
+    private void Update()
+    {
+        
+    }
+
+
+    public void AddDepartment()
+    {
+        if(newDeptIndex < departmentObjs.Length)
+        {
+            departmentObjs[newDeptIndex].SetActive(true);
+            newDeptIndex++;
+        }
+    }
+
+    public void SetMaxEmployees()
+    {
+        maxEmployees = 0;
+        foreach(GameObject department in departmentObjs)
+        {
+            if(department.activeInHierarchy == true)
+            {
+                maxEmployees += 2 * employeesPerFloor;
+            }
+        }
+    }
 }
